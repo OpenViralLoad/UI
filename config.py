cd4bb5a50781cac0a4c4217b07e21eea9b07354c
phuongho <phuongho43@gmail.com>
2017-02-04 01:01:59 -0800

    Added font-awesome icons; Setting up main page patient search backend

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 DEBUG = True
 DATABASE = 'medical_records.db'
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
+# SECRET_KEY = 'development key'
+# USERNAME = 'admin'
+# PASSWORD = 'default'
