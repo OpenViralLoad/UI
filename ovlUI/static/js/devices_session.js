24acd78dca3a92d33882c7d6499d206a6596834d
phuongho <phuongho43@gmail.com>
2017-02-08 23:14:58 -0800

    Finished function to add and maintain content in localStorage

--- ovlUI/static/js/devices_session.js
+++ ovlUI/static/js/devices_session.js
@@ -1,56 +1,60 @@
 function getParamFromQueryStr(name, url) {
-	if (!url) {
-		url = window.location.href;
-	}
-	name = name.replace(/[\[\]]/g, "\\$&");
-	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-		results = regex.exec(url);
-	if (!results) return null;
-	if (!results[2]) return '';
-	return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-// localStorage.clear();
-// New Input
-var add = {};
-add.device_type = getParamFromQueryStr('device_type');
-add.patient_id = getParamFromQueryStr('patient_id');
-
-// var devices = [{
-// 	"device_type": "ext1",
-// 	"patient_ids": ["1", "2"]
-// }, {
-// 	"device_type": "therm1",
-// 	"patient_ids": ["2", "4"]
-// }];
-
-// Stored Features
-if (localStorage.devices == null) {
-	console.log("null");
-	localStorage.devices = JSON.stringify([]);
-} else {
-	devices = JSON.parse(localStorage.devices);
-	console.log(devices);
+    if(!url) {
+        url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if(!results) return null;
+    if(!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
 
-var devices_list = [];
-devices.forEach(function(dev) {
-	devices_list.push(dev.device_type);
-});
-console.log(devices_list);
-if (devices_list.includes(add.device_type)) { // add more samples to existing tab
-	localStorage.devices.forEach(function(device) {
-		if (add.device_type == device.device_type) {
-			device.patient_ids.push(add.patient_id);
-		}
-	});
-} else { // add another tab
-	localStorage.devices.push(add);
+function addto_localStorage() {
+    // Fetch new input from url query string
+    var add = {};
+    add.device_type = getParamFromQueryStr('device_type');
+    add.patient_id = [getParamFromQueryStr('patient_id')];
+    // Fetch saved content from localStorage
+    if(localStorage.devices == null) {
+        localStorage.devices = JSON.stringify([]);
+        session = JSON.parse(localStorage.devices);
+    } else {
+        session = JSON.parse(localStorage.devices);
+    }
+    // Check if new samples should go to an existing device tab or new tab
+    var devices_list = [];
+    session.forEach(function(dev) {
+        devices_list.push(dev.device_type);
+    });
+
+    if(devices_list.includes(add.device_type)) { // add to existing tab
+        session.forEach(function(device) {
+            if(add.device_type == device.device_type) {
+                device.patient_id = device.patient_id.concat(add.patient_id);
+                // console.log(device);
+            }
+        });
+    } else { // add new tab
+        session.push(add);
+    }
+    localStorage.devices = JSON.stringify(session);
+    console.log(session);
+    return session;
 }
-console.log(devices);
-// console.log(localStorage.devices);
-// localStorage.setItem('devices', JSON.stringify(devices));
-// var session = JSON.parse(localStorage.getItem('devices'));
-// console.log(session);
+
+
+function clear_session() {
+    localStorage.clear();
+}
+
+
+function open_session() {
+    session = JSON.parse(localStorage.devices);
+    return session;
+}
+// addto_localStorage();
+// clear_session();
 // TODO: now generate tabs and content based on stored parameters (2-6-17)
 // document.getElementById("result").innerHTML = localStorage.session[0].patient_id;
