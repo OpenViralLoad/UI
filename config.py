d26c73a6f2d747b6c2a6a4a5a40da1cac39124cb
x6wu <x6wu@ucsd.edu>
2017-02-09 19:02:31 -0800

    nvm everything works

--- config.py
+++ config.py
@@ -3,9 +3,7 @@ from ovlUI import app
 
 DEBUG = True
 DATABASE = os.path.join(app.root_path, 'records.db')
-app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, '\static\img\profile_pics')
-#'D:\ME\OVL\ovlUI\static\img\profile_pics'
-#app.config['UPLOAD_FOLDER'] = '/static/img/profile_pics'
+app.config['UPLOAD_FOLDER'] = 'ovlUI/static/img/profile_pics/'
 app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg'])
 # SECRET_KEY = 'development key'
 # USERNAME = 'admin'
