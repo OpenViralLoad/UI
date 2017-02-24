382c84598c06385660b706319740884fc305e000
phuongho43 <phuongho43@gmail.com>
2017-02-23 18:21:06 -0800

    added samples panel for each device tab; serialport id text

--- ovlUI/static/js/devices_session.js
+++ ovlUI/static/js/devices_session.js
@@ -46,7 +46,7 @@ function addto_session() {
 
 
 function save_session() {
-	var tabs = {};
+	var session = {};
 	dev_tab_navs = document.getElementsByClassName("dev_tab_nav");
 	for (var i = 0; i < dev_tab_navs.length; i++) {
 		tab_nav = dev_tab_navs[i];
@@ -57,25 +57,10 @@ function save_session() {
 		for (var j = 0; j < children.length; j++) {
 			if (children[j].className == "tab_title") {
 				title = children[j].innerHTML;
-			} else if (children[j].className == "tab_progress") {
-				progress = children[j].innerHTML;
 			}
 		}
 	}
-	// tab_titles = document.getElementsByClassName("tab_title");
-	// tab_bars = document.getElementsByClassName("tab_progress");
-	// tab_samples = document.getElementsByClassName("tab_sample");
-	// tab_settings = document.getElementsByClassName("");
-	// for (var i = 0; i < tab_titles.length; i++) {
-	// 	console.log(tab_titles[i].childNodes);
-	// 	title = tab_titles[i].innerHTML;
-	// for (var j = 0; j < tab_titles[j].childNodes.length; j++) {
-	//
-	// }
-	// progress = tab_bars[i].getAttribute("style");
-	// tabs[title] = [progress];
-	// }
-	// console.log(tabs);
+
 }
 
 
